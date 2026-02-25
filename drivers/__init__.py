053a15a2099f229d20eb700b0e2a750af84425a5
JameyStiling <absherd@Absherds-MacBook-Pro.local>
2026-02-25 11:19:05 -0800

    prism initial agent setup, and scaffolding

---
+++ drivers/__init__.py
@@ -0,0 +1,7 @@
+"""
+Drivers package for Project Prism.
+
+This package will contain concrete driver implementations generated
+by the Agentic Factory, all built on top of the GenericBaseDriver.
+"""
+
